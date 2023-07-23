dd36b257ac695e1ffa9ca09fa598f7b0eb1dca4f
unknown <benbushman98@gmail.com>
2023-07-23 14:59:17 -0700

    added code and rewrote functionality.

---
+++ popup copy.js
@@ -0,0 +1,216 @@
+const localArrays = {
+  list1: [],
+  list2: [],
+  list3: [],
+};
+let tabArray = [];
+
+document.getElementById("newTab").addEventListener("click", function () {
+  let prompt = window.prompt("What is the name of your list?");
+  if(prompt === "" || prompt === null) {
+    console.log("fail")
+    prompt
+  } else {
+    tabArray.push(prompt);
+    localStorage.setItem("tabName", JSON.stringify(tabArray));
+    init(prompt);
+  }
+});
+
+function openTab(evt, tabName) {
+  const tabContent = document.getElementsByClassName("tab");
+  // console.log(tabContent)
+  for (let i = 0; i < tabContent.length; i++) {
+    // console.log("test")
+    tabContent[i].style.display = "none";
+  }
+
+  const tabLinks = document.getElementsByClassName("tabLinks");
+  // console.log(tabLinks)
+  // console.log(tabLinks.length)
+  for (let i = 0; i < tabLinks.length; i++) {
+    // console.log("remove")
+    tabLinks[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  if (evt) {
+    // console.log("added")
+    evt.currentTarget.classList.add("active");
+  }
+}
+
+function setNewWebsiteToLocalStorage(tabName, value) {
+  if (value.trim() !== "") {
+    if (!localArrays[tabName]) {
+      localArrays[tabName] = [];
+    }
+    localArrays[tabName].push(value);
+    localStorage.setItem(
+      `Favorite:${tabName}`,
+      JSON.stringify(localArrays[tabName])
+    );
+    document.getElementById("entry").value = "";
+    init(tabName); // Call init() to populate the list after adding a new item
+  }
+}
+
+function init(tabName) {
+  const list = document.getElementById(`favList${tabName}`);
+  const setArray = localStorage.getItem(`Favorite:${tabName}`);
+  const buttonList = document.querySelector(".tabLinksContainer");
+  const setTabArray = localStorage.getItem("tabName");
+
+  if (setTabArray) {
+    tabArray = JSON.parse(setTabArray);
+  } else {
+    tabArray = [];
+  }
+
+  if (setArray) {
+    localArrays[tabName] = JSON.parse(setArray);
+  } else {
+    localArrays[tabName] = [];
+  }
+  buttonList.innerHTML = "";
+  if (list === null) {
+    console.log("null")
+  } else {
+  list.innerHTML = "";
+  }
+  for (let i = 0; i < tabArray.length; i++) {
+    let name = tabArray[i];
+    let container = document.querySelector(".tabLinksContainer");
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.classList.add("tabDeleteButton")
+    deleteButton.addEventListener("click", function () {
+      deleteTabFromLocalStorage(name, i);
+    });
+
+    let newTab = document.createElement("button");
+    newTab.classList.add("tabLinks");
+    newTab.setAttribute("data-tab", `list${i}`);
+    newTab.textContent = name;
+    newTab.appendChild(deleteButton);
+    // console.log(newTab)
+    container.appendChild(newTab);
+  }
+
+  for (let i = 0; i < localArrays[tabName].length; i++) {
+    const url = localArrays[tabName][i];
+    const modifiedUrl = removeCharactersAfterDomain(url); // Call the function to modify the URL
+
+    let listItem = document.createElement("li");
+    listItem.textContent = modifiedUrl; // Use the modified URL as the text content
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function () {
+      deleteFromLocalStorage(tabName, i);
+    });
+
+    listItem.appendChild(deleteButton);
+    list.appendChild(listItem);
+  }
+}
+
+function deleteTabFromLocalStorage(tabName, index) {
+  tabArray.splice(index, 1);
+  localStorage.setItem(
+    `tabName`,
+    JSON.stringify(tabArray)
+  );
+  init(tabName);
+}
+function deleteFromLocalStorage(tabName, index) {
+  localArrays[tabName].splice(index, 1);
+  localStorage.setItem(
+    `Favorite:${tabName}`,
+    JSON.stringify(localArrays[tabName])
+  );
+  init(tabName);
+}
+
+function removeCharactersAfterDomain(url) {
+  // Regular expression to match the domain extension and everything after it
+  const regex = /^(https?:\/\/)?([^/?#\s]+)(.*)/;
+
+  // Extract the domain extension and everything before it
+  const [, protocol, domain, rest] = url.match(regex) || [];
+
+  // Reconstruct the URL with only the protocol and domain
+  const modifiedUrl = (protocol ? protocol : "") + domain;
+
+  return modifiedUrl;
+}
+
+function openFavorites(tabName) {
+  console.log(tabName.split(" "));
+
+  const getReady = localStorage.getItem(
+    `Favorite:${tabName.split("favList")[1]}`
+  );
+  console.log(getReady);
+  const items = getReady ? JSON.parse(getReady) : [];
+
+  if (items.length === 0) {
+    console.log("No items in the list.");
+    return; // Nothing to open
+  }
+
+  for (let i = 0; i < items.length; i++) {
+    console.log(`Opening URL: ${items[i]}`);
+    window.open(`${items[i]}`, "_blank");
+  }
+}
+
+async function initializeDefaultTab() {
+  const activeTab = document.querySelector(".tabLinks.active");
+  const defaultTab = activeTab ? activeTab.getAttribute("data-tab") : "list0";
+  // console.log(defaultTab)
+  // console.log("Initializing default tab...")
+  // console.log(defaultTab)
+  openTab(null, defaultTab); // Open the default tab
+  init(defaultTab); // Initialize the default tab's list
+}
+
+// Attach event listeners after the DOM has loaded
+document.addEventListener("DOMContentLoaded", async function  () {
+  await initializeDefaultTab(); // Initialize the default tab's list
+  const tabLinks = document.getElementsByClassName("tabLinks");
+  // console.log(tabLinks)
+  // console.log(tabLinks.length)
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener("click", function (event) {
+      console.log("test")
+      const tabName = event.target.getAttribute("data-tab");
+      openTab(event, tabName);
+      init(tabName); // Call init() to populate the list when switching tabs
+    });
+  }
+
+
+  document
+    .getElementById("submitButton")
+    .addEventListener("click", function () {
+      const activeTab = document.querySelector(".tabLinks.active");
+      if (activeTab) {
+        console.log("activeTab")
+        const tabName = activeTab.getAttribute("data-tab");
+        const value = document.getElementById("entry").value;
+        setNewWebsiteToLocalStorage(tabName, value);
+      }
+    });
+
+  });
+  const openFavsButtons = document.querySelectorAll("[id^='openFavs']");
+  console.log(openFavsButtons)
+  openFavsButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      console.log("test click")
+      const tabName = event.target.getAttribute("data-list");
+      openFavorites(`favList${tabName}`);
+    });
+  });
--- popup.js
+++ popup.js
@@ -1,148 +1,182 @@
+// Definitions of Global Variables
+let tabArray = [];
 const localArrays = {
-    list1: [],
-    list2: [],
-    list3: [],
-  };
-  
-  function openTab(evt, tabName) {
-    const tabContent = document.getElementsByClassName("tab");
-    for (let i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-    }
-  
-    const tabLinks = document.getElementsByClassName("tabLinks");
-    for (let i = 0; i < tabLinks.length; i++) {
-      tabLinks[i].classList.remove("active");
-    }
-  
-    document.getElementById(tabName).style.display = "block";
-    if (evt) {
-      evt.currentTarget.classList.add("active");
-    }
-  }
-  
-  function setNewWebsiteToLocalStorage(tabName, value) {
-    if (value.trim() !== "") {
-      if (!localArrays[tabName]) {
-        localArrays[tabName] = [];
-      }
-      localArrays[tabName].push(value);
-      localStorage.setItem(`Favorite:${tabName}`, JSON.stringify(localArrays[tabName]));
-      document.getElementById("entry").value = "";
-      init(tabName); // Call init() to populate the list after adding a new item
-    }
-  }
-  
-  function init(tabName) {
-    const list = document.getElementById(`favList${tabName}`);
-    const setArray = localStorage.getItem(`Favorite:${tabName}`);
-  
-    if (setArray) {
-      localArrays[tabName] = JSON.parse(setArray);
+  0: [],
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+};
+
+// Definition of Global Clickable Event Listeners.
+document.getElementById("newTab").addEventListener("click", createNewTab);
+document.getElementById("deleteTab").addEventListener("click", deleteTab);
+document.getElementById("submitButton").addEventListener("click", addToList);
+document.getElementById("id_dropdown").onchange = renderListOnly;
+document.getElementById("openTab").addEventListener("click", openFavorites)
+
+function createNewTab() {
+  let prompt = window.prompt("What is the name of your list?");
+  if (prompt === "" || prompt === null) {
+    console.log("fail")
+    alert("Please Enter a Valid Name")
+  } else {
+    const setTabArray = localStorage.getItem("tabName");
+    if (setTabArray !== null && setTabArray.includes(prompt)) {
+      alert("List Name Already in Use. Please Name Something Different.")
     } else {
-      localArrays[tabName] = [];
+      tabArray.push(prompt);
+      localStorage.setItem("tabName", JSON.stringify(tabArray));
+      renderTabNameToScreenFromLocalStorage();
     }
-  
-    list.innerHTML = "";
-  
-    for (let i = 0; i < localArrays[tabName].length; i++) {
-      const url = localArrays[tabName][i];
-      const modifiedUrl = removeCharactersAfterDomain(url); // Call the function to modify the URL
-  
+  }
+};
+
+function deleteTab() {
+  var element = document.getElementById("id_dropdown");
+  let confirm = window.confirm(`Are you sure you want to delete your ${element.value} tab?`)
+  // console.log(confirm)
+  if (confirm) {
+    tabArray.splice(element.selectedIndex, 1);
+    localStorage.setItem(
+      `tabName`,
+      JSON.stringify(tabArray)
+      );
+      renderTabNameToScreenFromLocalStorage();
+    } else {
+      return
+    }
+}
+function addToList() {
+  var element = document.getElementById("id_dropdown");
+  let tabName = document.getElementById("id_dropdown").value;
+  let listName = document.getElementById("entry");
+  if (listName.value === "") {
+    alert("Please enter a valid URL (e.g. https://google.com)")
+    return
+  }  
+  localArrays[element.selectedIndex].push(listName.value)
+  localStorage.setItem(tabName, JSON.stringify(localArrays[element.selectedIndex]));
+  renderListtoScreenFromLocalStorage();
+  listName.value = "";
+}
+
+function init(param) {
+  renderTabNameToScreenFromLocalStorage();
+  renderListtoScreenFromLocalStorage();
+}
+function renderListOnly() {
+  renderListtoScreenFromLocalStorage();
+
+}
+
+// Get the Tab Names from Local Storage and Render Them to the Screen
+function renderTabNameToScreenFromLocalStorage() {
+  const buttonList = document.querySelector(".tabLinksContainer");
+  const setTabArray = localStorage.getItem("tabName");
+
+  if (setTabArray) {
+    tabArray = JSON.parse(setTabArray);
+  } else {
+    tabArray = [];
+  }
+  buttonList.innerHTML = "";
+
+  for (let i = 0; i < tabArray.length; i++) {
+    let name = tabArray[i];
+    let container = document.querySelector(".tabLinksContainer");
+
+    let newTab = document.createElement("option");
+    newTab.classList.add("tabLinks");
+    newTab.setAttribute("value", name);
+    newTab.textContent = name;
+    container.appendChild(newTab);
+  }
+}
+function renderListtoScreenFromLocalStorage() {
+  const list = document.getElementById("list");
+  var element = document.getElementById("id_dropdown");
+  let tabName = document.getElementById("id_dropdown").value;
+  const setArray = localStorage.getItem(`${tabName}`);
+
+  list.innerHTML = ""; // Clear the list before rendering the new items
+
+  if (setArray) {
+    localArrays[element.selectedIndex] = JSON.parse(setArray);
+    for (let i = 0; i < localArrays[element.selectedIndex].length; i++) {
+      const url = localArrays[element.selectedIndex][i];
+      const modifiedUrl = removeCharactersAfterDomain(url);
       let listItem = document.createElement("li");
       listItem.textContent = modifiedUrl; // Use the modified URL as the text content
   
       let deleteButton = document.createElement("button");
-      deleteButton.textContent = "Delete";
+      deleteButton.textContent = "x";
       deleteButton.addEventListener("click", function () {
-        deleteFromLocalStorage(tabName, i);
+        console.log("delete List")
+        deleteFromLocalStorage(element, i);
       });
   
       listItem.appendChild(deleteButton);
       list.appendChild(listItem);
     }
   }
-  
-  function deleteFromLocalStorage(tabName, index) {
-    localArrays[tabName].splice(index, 1);
-    localStorage.setItem(`Favorite:${tabName}`, JSON.stringify(localArrays[tabName]));
-    init(tabName);
-  }
-  
-  function removeCharactersAfterDomain(url) {
-    // Regular expression to match the domain extension and everything after it
-    const regex = /^(https?:\/\/)?([^/?#\s]+)(.*)/;
-  
-    // Extract the domain extension and everything before it
-    const [, protocol, domain, rest] = url.match(regex) || [];
-  
-    // Reconstruct the URL with only the protocol and domain
-    const modifiedUrl = (protocol ? protocol : '') + domain;
-  
-    return modifiedUrl;
-  }
-  
-  function openFavorites(tabName) {
-    console.log(tabName.split(' '))
+}
 
-    const getReady = localStorage.getItem(`Favorite:${tabName.split("favList")[1]}`);
-    console.log(getReady)
-    const items = getReady ? JSON.parse(getReady) : [];
-  
-    if (items.length === 0) {
-      console.log("No items in the list.");
-      return; // Nothing to open
-    }
-  
-    for (let i = 0; i < items.length; i++) {
-      console.log(`Opening URL: ${items[i]}`);
-      window.open(`${items[i]}`, "_blank");
-    }
+function removeCharactersAfterDomain(url) {
+  // Regular expression to match the domain extension
+  const regex = /^(https?:\/\/)?([^/?#\s]+)/;
+
+  // Extract the domain extension
+  const [, protocol, domain] = url.match(regex) || [];
+
+  // Reconstruct the URL with only the protocol and domain
+  const modifiedUrl = (protocol ? protocol : "") + domain;
+
+  return modifiedUrl;
+}
+
+// Function to be able to delete tab name or list name from localstorage
+function deleteTabFromLocalStorage(index) {
+  tabArray.splice(index, 1);
+  localStorage.setItem(
+    `tabName`,
+    JSON.stringify(tabArray)
+  );
+  renderTabNameToScreenFromLocalStorage();
+}
+function deleteFromLocalStorage(element, index) {
+  localArrays[element.selectedIndex].splice(index, 1);
+  localStorage.setItem(
+    `${element.value}`,
+    JSON.stringify(localArrays[element.selectedIndex])
+  );
+  renderListtoScreenFromLocalStorage();
+}
+
+function openFavorites() {
+  let tabName = document.getElementById("id_dropdown").value;
+  console.log(tabName.split(" "));
+
+  const getReady = localStorage.getItem(
+    `${tabName}`
+  );
+  console.log(getReady);
+  const items = getReady ? JSON.parse(getReady) : [];
+
+  if (items.length === 0) {
+    console.log("No items in the list.");
+    return; // Nothing to open
   }
-  
-  
-  async function initializeDefaultTab() {
-    const activeTab = document.querySelector(".tabLinks.active");
-    const defaultTab = activeTab ? activeTab.getAttribute("data-tab") : "list1";
-  
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(); // Simulate a short delay to ensure local storage data retrieval
-      }, 100);
-    });
-  
-    openTab(null, defaultTab); // Open the default tab
-    init(defaultTab); // Initialize the default tab's list
+
+  for (let i = 0; i < items.length; i++) {
+    console.log(`Opening URL: ${items[i]}`);
+    window.open(`${items[i]}`, "_blank");
   }
-  
-  // Attach event listeners after the DOM has loaded
-  document.addEventListener("DOMContentLoaded", function () {
-    const tabLinks = document.getElementsByClassName("tabLinks");
-    for (let i = 0; i < tabLinks.length; i++) {
-      tabLinks[i].addEventListener("click", function (event) {
-        const tabName = event.target.getAttribute("data-tab");
-        openTab(event, tabName);
-        init(tabName); // Call init() to populate the list when switching tabs
-      });
-    }
-  
-    initializeDefaultTab(); // Initialize the default tab's list
-  
-    document.getElementById("submitButton").addEventListener("click", function () {
-      const activeTab = document.querySelector(".tabLinks.active");
-      if (activeTab) {
-        const tabName = activeTab.getAttribute("data-tab");
-        const value = document.getElementById("entry").value;
-        setNewWebsiteToLocalStorage(tabName, value);
-      }
-    });
-  
-    const openFavsButtons = document.querySelectorAll("[id^='openFavs']");
-    openFavsButtons.forEach((button) => {
-      button.addEventListener("click", function (event) {
-        const tabName = event.target.getAttribute("data-list");
-        openFavorites(`favList${tabName}`);
-      });
-    });
-  });
-  
+}
+
+init();
